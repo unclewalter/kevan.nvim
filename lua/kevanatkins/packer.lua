2d21fe16e5135f6b7f4b88382339cd1b0bd190f8
Kevan Atkins <kevan.atkins@gmail.com>
2023-12-11 14:28:22 +1100

    Adding nvim-surround

--- lua/kevanatkins/packer.lua
+++ lua/kevanatkins/packer.lua
@@ -37,4 +37,14 @@ return require('packer').startup(function(use)
 		}
 	}
     use 'voldikss/vim-floaterm'
+
+    use({
+        "kylechui/nvim-surround",
+        tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+    })
 end)
