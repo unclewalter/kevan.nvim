8db918eacaf77af94c086f5d24580a5bed71ffec
Kevan Atkins <kevan.atkins@gmail.com>
2025-04-24 03:53:25 +1000

    Which key? Why not

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,10 +1,22 @@
 return {
 	{'akinsho/toggleterm.nvim', version = "*", config = true},
-	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
+	{'williamboman/mason.nvim'},
+	{'neovim/nvim-lspconfig'},
 	{
-		'williamboman/mason.nvim',
-	},
-	{
-		'neovim/nvim-lspconfig',
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		opts = {
+			
+		},
+		keys = {
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Buffer Local Keymaps (which-key)",
+			},
+		},
 	}
 }
